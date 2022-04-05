fc0ccf9764b72b0b631a6c93545d946166251003
tiffpereira <tiffanympereira@gmail.com>
2022-04-04 21:43:28 -0400

    CRUD functionality

--- client/src/components/LowerWorkouts.js
+++ client/src/components/LowerWorkouts.js
@@ -5,6 +5,14 @@ import axios from 'axios'
 
 const LowerWorkouts = () => { 
     const [lower, setLower] = useState({})
+    const [newLower, setNewLower] = useState({
+        title: '',
+        description: '',
+        time_frame: '',
+        url: ''
+    })
+    const [click, setClick] = useState(0)
+
 
     const getLower = async () => {
         const res = await axios.get('http://localhost:3001/lowerbodyworkouts')
@@ -14,10 +22,43 @@ const LowerWorkouts = () => {
 
     useEffect(() => {
         getLower()
-    }, [])
+    }, [click])
 
-    if (lower[0]) {
+
+const handleChange = (event) => {
+    const {input, value} = event.target
+    setNewLower((lowerWorkouts) => {
+        return {...lowerWorkouts, [input]: value}
+    })
+ } 
+
+const handleAdd = async (event) => {
+    event.preventDefault()
+    const lowerWorkoutList = {
+        title: newLower.title,
+        description: newLower.description,
+        time_frame: newLower.time_frame,
+        url: newLower.url 
+    }
+    await axios.post('http://localhost:3001/lowerbodyworkouts', lowerWorkoutList)
+        setClick((prevState) => prevState + 1)
+        setNewLower({
+            title: '',
+            description: '',
+            time_frame: '',
+            url: '' 
+})}
+
+const handleDelete = async (event) => {
+    let lowerId = lower._id
+    event.preventDefault()
+    await axios.delete(`http://localhost:3001/lowerbodyworkouts/${lowerId}`)
+    setClick((prevState) => prevState + 1)
+}
+
+if (lower[0]) {
         return (
+        <div>
             <div className = 'workout-list'>
                 {lower.map((lowerWorkouts) => (
                     <div>
@@ -25,12 +66,29 @@ const LowerWorkouts = () => {
                         <h3>Time frame: {lowerWorkouts.time_frame}</h3>
                         <h4 id ='workout-description'>Workout description: {lowerWorkouts.description}</h4>
                         <h4>{lowerWorkouts.url}</h4>
+                        <button onClick={handleDelete}> Delete Workout</button>
                     </div>
                 ))}
             </div>
+
+            <div className="newworkoutform">
+            <h3 className='newworkouttitle'>Add your own workout here!</h3>
+                <form>
+                    <div>
+                        <input type='text' onChange={handleChange} name="title" value={newLower.title} placeholder="Workout title"></input>
+
+                        <input type='text' onChange={handleChange} name="timeframe" value={newLower.time_frame} placeholder="Workout time frame"></input>
+
+                        <textarea type='text' onChange={handleChange} name="description" value={newLower.description} placeholder="Workout description..." ></textarea>
+
+                        <input type='text' onChange={handleChange} name="url" value={newLower.url} placeholder="Workout url"></input>
+
+                    </div>
+                <button onClick={handleAdd}>Add Workout</button>
+                </form>
+            </div>
+        </div>  
     )
-}       else {
-    return <h1>Page loading...</h1>
 }
 }
 
--- client/src/components/UpperWorkouts.js
+++ client/src/components/UpperWorkouts.js
@@ -5,6 +5,13 @@ import axios from 'axios'
 
 const UpperWorkouts = () => { 
     const [upper, setUpper] = useState({})
+    const [newUpper, setNewUpper] = useState({
+        title: '',
+        description: '',
+        time_frame: '',
+        url: ''
+    })
+    const [click, setClick] = useState(0)
 
     const getUpper = async () => {
         const res = await axios.get('http://localhost:3001/upperbodyworkouts')
@@ -14,10 +21,44 @@ const UpperWorkouts = () => {
 
     useEffect(() => {
         getUpper()
-    }, [])
+    }, [click])
+
+
+    const handleChange = (event) => {
+        const {input, value} = event.target
+        setNewUpper((Upper) => {
+            return {...Upper, [input]: value}
+        })
+     } 
+    
+    const handleAdd = async (event) => {
+        event.preventDefault()
+        const upperWorkoutList = {
+            title: newUpper.title,
+            description: newUpper.description,
+            time_frame: newUpper.time_frame,
+            url: newUpper.url 
+        }
+        await axios.post('http://localhost:3001/upperbodyworkouts', upperWorkoutList)
+            setClick((prevState) => prevState + 1)
+            setNewUpper({
+                title: '',
+                description: '',
+                time_frame: '',
+                url: '' 
+    })}
+    
+    const handleDelete = async (event) => {
+        let upperId = upper._id
+        event.preventDefault()
+        await axios.delete(`http://localhost:3001/upperbodyworkouts/${upperId}`)
+        setClick((prevState) => prevState + 1)
+    }
+
 
     if (upper[0]) {
         return (
+            <div>
             <div className = 'workout-list'>
                 {upper.map((upperWorkouts) => (
                     <div>
@@ -25,13 +66,30 @@ const UpperWorkouts = () => {
                         <h3>Time frame: {upperWorkouts.time_frame}</h3>
                         <h4 id ='workout-description'>Workout description: {upperWorkouts.description}</h4>
                         <h4>{upperWorkouts.url}</h4>
+                        <button onClick={handleDelete}> Delete Workout</button>
                     </div>
                 ))}
             </div>
-    )
-}       else {
-    return <h1>Page loading...</h1>
+    
+            <div className="newworkoutform">
+                <h3 className='newworkouttitle'>Add your own workout here!</h3>
+                    <form>
+                        <div>
+                            <input type='text' onChange={handleChange} name="title" value={newUpper.title} placeholder="Workout title"></input>
+
+                            <input type='text' onChange={handleChange} name="timeframe" value={newUpper.time_frame} placeholder="Workout time frame"></input>
+
+                            <textarea type='text' onChange={handleChange} name="description" value={newUpper.description} placeholder="Workout description..." ></textarea>
+
+                            <input type='text' onChange={handleChange} name="url" value={newUpper.url} placeholder="Workout url"></input>
+
+                        </div>
+                            <button onClick={handleAdd}>Add Workout</button>
+                    </form>
+            </div>
+            </div>  
+)}
 }
-}
+
 
 export default UpperWorkouts
